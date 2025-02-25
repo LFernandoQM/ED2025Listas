c2d78f7715d8622e0f2c45fb22a72aa5877a1593
Quimera <Administrator@QUIMERA>
2025-02-24 21:13:29 -0700

    Se añadieron comentarios acerca de el funcionamiento de cada metodo y linea de el codigo.

--- ListasSimplementeLigadas/Lista.cs
+++ ListasSimplementeLigadas/Lista.cs
@@ -1,66 +1,81 @@
-﻿using System.Text;
+﻿using System.Text; // Esto permite utilizar la clase StringBuilder
 
-namespace ListasSimplementeLigadas
+namespace ListasSimplementeLigadas // Nombre del proyecto
 {
+    // Es la clase de una lista simplemente ligada
     internal class Lista
     {
+        // Nodo inicial de la lista
         private Nodo _nodoInicial;
 
-        public Lista() 
+        // Constructor que inicializa la lista con un nodo vacío
+        public Lista()
         {
             _nodoInicial = new Nodo();
         }
 
+        // Metodo para agregar un nuevo dato a la lista
         public void Agregar(string dato)
         {
             Nodo nodoActual = _nodoInicial;
 
+            // Recorre la lista hasta encontrar el ultimo nodo
             while (nodoActual.Siguiente != null)
             {
                 nodoActual = nodoActual.Siguiente;
             }
 
+            // Crear un nuevo nodo con el dato proporcionado
             Nodo nodoNuevo = new Nodo();
             nodoNuevo.Dato = dato;
 
+            // Enlazar el nuevo nodo al final de la lista
             nodoActual.Siguiente = nodoNuevo;
         }
 
-        public bool EstaVacio() 
+        // Metodo que verifica si la lista esta vacía
+        public bool EstaVacio()
         {
-            return (_nodoInicial.Siguiente == null); 
+            return (_nodoInicial.Siguiente == null);
         }
 
+        // Metodo para vaciar la lista
         public void Vaciar()
         {
             _nodoInicial.Siguiente = null;
         }
 
-        public Nodo? Buscar(string dato) 
+        // Metodo para buscar un dato en la lista
+        public Nodo? Buscar(string dato)
         {
             if (!EstaVacio())
             {
                 Nodo nodoActual = _nodoInicial;
 
+                // Recorre la lista en busca del dato
                 while (nodoActual.Siguiente != null)
                 {
                     nodoActual = nodoActual.Siguiente;
 
+                    // Si se encuentra el dato... retorna el nodo correspondiente
                     if (nodoActual.Dato == dato)
                     {
                         return nodoActual;
                     }
                 }
             }
+            // Si no se encuentra el dato... retorna null
             return null;
         }
 
+        // Metodo para buscar el nodo anterior al que contiene el dato proporcionado
         private Nodo? BuscarAnterior(string dato)
         {
             if (!EstaVacio())
             {
                 Nodo nodoActual = _nodoInicial;
 
+                // Recorre la lista en busca del nodo anterior
                 while (nodoActual.Siguiente != null)
                 {
                     if (nodoActual.Siguiente.Dato == dato)
@@ -71,21 +86,26 @@ namespace ListasSimplementeLigadas
                     nodoActual = nodoActual.Siguiente;
                 }
             }
+            // Si no se encuentra el nodo anterior...retorna null
             return null;
         }
 
+        // Metodo para eliminar un nodo que contiene el dato proporcionado
         public void Eliminar(string dato)
         {
             if (!EstaVacio())
             {
+                // Buscar el nodo que contiene el dato
                 Nodo? nodoActual = Buscar(dato);
 
                 if (nodoActual != null)
                 {
+                    // Buscar el nodo anterior al que contiene el dato
                     Nodo? nodoAnterior = BuscarAnterior(dato);
 
                     if (nodoAnterior != null)
                     {
+                        // Eliminar el nodo de la lista
                         nodoAnterior.Siguiente = nodoActual.Siguiente;
                         nodoActual.Siguiente = null;
                     }
@@ -93,11 +113,13 @@ namespace ListasSimplementeLigadas
             }
         }
 
-        public string ObtenerValores() 
+        // Metodo para obtener todos los valores de la lista en forma de cadena
+        public string ObtenerValores()
         {
             StringBuilder datos = new StringBuilder();
             Nodo nodoActual = _nodoInicial;
 
+            // Recorre la lista y agrega cada dato a la cadena
             while (nodoActual.Siguiente != null)
             {
                 nodoActual = nodoActual.Siguiente;
@@ -105,6 +127,7 @@ namespace ListasSimplementeLigadas
                 datos.AppendLine(nodoActual.Dato);
             }
 
+            // Retorna la cadena con todos los datos de la lista
             return datos.ToString();
         }
     }
